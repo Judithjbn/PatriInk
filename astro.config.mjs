2ae5be3c9fc70f17aa651fb0c3f5f144df7356f3
Judith <jbarrilnavarro@gmail.com>
2025-01-19 01:21:06 +0100

    limpieza de archivos

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://astroship.web3templates.com",
+  site: "https://patrinktattoo.netlify.app/",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
   build: {
     rollupOptions: {
